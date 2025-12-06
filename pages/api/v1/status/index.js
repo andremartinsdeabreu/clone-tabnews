2b13355df8e0fb57c6bbe6de6d8456ee1d7123cc
andremartinsdeabreu <amartins.net@outlook.com>
2025-12-05 23:30:35 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,43 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("show server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "show max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "select count(pid)::int from pg_stat_activity where datname = $1",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  const databaseActivedConnectionsResult = await database.query({
+    text: "select count(pid)::int from pg_stat_activity where datname = $1 and state = $2;",
+    values: [databaseName, "active"],
+  });
+  const databaseActivedConnectionsValue =
+    databaseActivedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    chave: "é teste api!",
+    updated_at: updatedAt,
+    dependendencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+        actived_connections: databaseActivedConnectionsValue,
+      },
+    },
   });
 }
 
